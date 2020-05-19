604991c1029de7f8d0b65ee6c5cb6ef4f172b2c9
Rosamund <rsmnd.mthr@gmail.com>
2020-05-19 11:10:01 +0200

    MVP finally created

--- main.py
+++ main.py
@@ -1,39 +1,34 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QSpinBox, QDoubleSpinBox, QLabel, QLineEdit, QTextEdit, QPushButton, QGridLayout, QButtonGroup
-#from functions import functionAWindow, functionBWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QGridLayout
 
-# import fractions module - functionA must be decimal!
-
-
-class mainWindow(QWidget): #QMainWindow
+class mainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.resize(300, 200)
+        self.resize(200, 180)
         self.setWindowTitle("Percentage Calculator")
 
         # for functionA:
-        self.titleFuncA = QLabel("Find z% of y, i.e. x")
-        self.labelA1 = QLabel("z (enter decimal):")
-        self.inputA1 = QDoubleSpinBox()
-        #self.inputA1 = QTextEdit()
+        self.titleFuncA = QLabel("x = z% of y")
+        self.labelA1 = QLabel("z:")
+        self.inputA1 = QLineEdit()
         self.labelA2 = QLabel("y:")
-        self.inputA2 = QSpinBox()
-    #    self.inputA2 = QTextEdit()
+        self.inputA2 = QLineEdit()
         self.button1 = QPushButton("Calculate!")
         self.button1.clicked.connect(self.functionA)
-        self.result1 = QTextEdit()
+        self.result1 = QLabel()
 
         # for functionB:
         self.titleFuncB = QLabel("z = x% of y")
         self.labelB1 = QLabel("z:")
-        self.inputB1 = QTextEdit()
+        self.inputB1 = QLineEdit()
         self.labelB2 = QLabel("y:")
-        self.inputB2 = QTextEdit()
+        self.inputB2 = QLineEdit()
         self.button2 = QPushButton("Calculate!")
-#        self.button2.clicked.connect(self.functionB)
-#       self.result2 = QTextEdit()
+        self.button2.clicked.connect(self.functionB)
+        self.result2 = QLabel()
 
         grid = QGridLayout()
+        # functionA layout:
         grid.addWidget(self.titleFuncA, 1, 0)
         grid.addWidget(self.labelA1, 2, 0)
         grid.addWidget(self.inputA1, 2, 1)
@@ -41,43 +36,45 @@ class mainWindow(QWidget): #QMainWindow
         grid.addWidget(self.inputA2, 3, 1)
         grid.addWidget(self.button1, 4, 0)
         grid.addWidget(self.result1, 5, 0)
-        grid.addWidget(self.titleFuncB, 6, 0)
-        grid.addWidget(self.labelB1, 7, 0)
-        grid.addWidget(self.inputB1, 7, 1)
-        grid.addWidget(self.labelB2, 8, 0)
-        grid.addWidget(self.inputB2, 8, 1)
-        grid.addWidget(self.button2, 9, 0)
-
-
+        # functionB layout:
+        grid.addWidget(self.titleFuncB, 7, 0)
+        grid.addWidget(self.labelB1, 8, 0)
+        grid.addWidget(self.inputB1, 8, 1)
+        grid.addWidget(self.labelB2, 9, 0)
+        grid.addWidget(self.inputB2, 9, 1)
+        grid.addWidget(self.button2, 10, 0)
+        grid.addWidget(self.result2, 11, 0)
 
         self.setLayout(grid)
         self.show()
 
-    # write functionA:
-    def functionA(self): # x, y ?
-        z = float(self.inputA1.text())
-    #    z = float(self.inputA1.toPlainText())
-        if z >= 1:
-            return "Please enter a valid decimal"
-        else:
-            y = int(self.inputA2.text())
-            z_as_int = z * 10
-            x  = z_as_int * y
-            x_string = f"{x}% of {y} is {z_as_int}"
+
+    # functionA: x = z% of y
+    def functionA(self):
+        x = float(self.inputA1.text())
+        y = float(self.inputA2.text())
+        if x and y:
+            x_float = x / 100
+            z  = x_float * y
+            x_string = f"x = {x}\ni.e. {x}% of {y} is {z}"
             self.result1.setText(x_string)
-            # add clear!
+            self.inputA1.clear()
+            self.inputA2.clear()
+        else:
+            return "Please check both fields are complete"
 
-
-    # write functionB:
+    # functionB: z = x% of y
     def functionB(self):
-        pass
-
-#    def openNew(self): # openNewA?
-#        self.n = openNew()
-#        self.n.show()
-
-    #    self.button1.clicked.connect(self.openNew)
-    #    self.button2.clicked.connect(self.openNew)
+        z = float(self.inputB1.text())
+        y = float(self.inputB2.text())
+        if z and y:
+            x = z * 100 / y
+            x_string = f"x = {x}\ni.e. {z} is {x}% of {y}"
+            self.result2.setText(x_string)
+            self.inputB1.clear()
+            self.inputB2.clear()
+        else:
+            return "Please check both fields are complete"
 
 
 if __name__ == '__main__':
